886c171fec94ca4981e007ce14b2edcaf9976fd7
fancn <fanyj@yikeer.com>
2017-04-16 22:29:10 +0800

    simple code coverage added

--- components/Counter/test/sagas.test.js
+++ components/Counter/test/sagas.test.js
@@ -1,12 +1,24 @@
-import { incrementAsync } from '../sagas'
+import { incrementAsync, helloSaga, watchIncrementAsync } from '../sagas'
 import { delay } from 'redux-saga'
-import { put, call } from 'redux-saga/effects'
+import { put, call, takeEvery } from 'redux-saga/effects'
 
-test('incrementAsync Saga tes', () => {
+test('incrementAsync Saga test', () => {
   const gen = incrementAsync()
 
   expect(gen.next().value).toEqual(call(delay, 1000))
   expect(gen.next().value).toEqual(put({ type: 'INCREMENT' }))
   expect(gen.next()).toEqual({value: undefined, done: true})
+})
 
+test('helloSaga Saga test', () => {
+  const gen = helloSaga()
+
+  expect(gen.next()).toEqual({value: undefined, done: true})
+})
+
+test('watchIncrementAsync Saga test', () => {
+  const gen = watchIncrementAsync()
+
+  expect(gen.next()).toEqual(takeEvery('INCREMENT_ASYNC', incrementAsync))
+  expect(gen.next()).toEqual({value: undefined, done: true})
 })
