c848749372123c3f3f34c8acbf460da7a9968b32
fancn <fanyj@yikeer.com>
2017-04-19 12:09:01 +0800

    non blocking login flow

--- components/Login/sagas.js
+++ components/Login/sagas.js
@@ -1,5 +1,37 @@
-import { takeLatest, take, put, call } from 'redux-saga/effects'
+import { takeLatest, take, put, call, fork, cancel, cancelled } from 'redux-saga/effects'
 import { login, logout } from '../../api'
+
+// export function* loginRequest(username, password, verificationCode) {
+//   try{
+//     const token = yield call(login, username, password, verificationCode)
+//     yield put({type:'LOGIN_SUCCEED', token})
+//     return token
+//   }catch (error){
+//     yield put({type:'LOGIN_FAILED', error})
+//   }
+// }
+//
+// export function* logoutRequest() {
+//   try{
+//     yield call(logout)
+//     yield put({type:'LOGOUT_SUCCEED'})
+//   }catch (error){
+//     yield put({type:'LOGOUT_FAILED'})
+//   }
+// }
+//
+// export function* loginFlow() {
+//   while(true){
+//     const { username, password, verificationCode } = yield take('LOGIN_REQUEST')
+//     // if loginRequest is failed, it will return an undefined value
+//     // which will skip next process and wait for another LOGIN_REQUEST action
+//     const token = yield call(loginRequest, username, password, verificationCode)
+//     if(token){
+//       yield take('LOGOUT_REQUEST')
+//       yield call(logoutRequest)
+//     }
+//   }
+// }
 
 export function* loginRequest(username, password, verificationCode) {
   try{
@@ -8,6 +40,10 @@ export function* loginRequest(username, password, verificationCode) {
     return token
   }catch (error){
     yield put({type:'LOGIN_FAILED', error})
+  }finally {
+    if (yield cancelled()) {
+      console.log('loginRequest task has been cancelled')
+    }
   }
 }
 
@@ -20,14 +56,15 @@ export function* logoutRequest() {
   }
 }
 
-export function* loginFlow() {
+export function* loginNonBlockingFlow() {
   while(true){
     const { username, password, verificationCode } = yield take('LOGIN_REQUEST')
-    const token = yield call(loginRequest, username, password, verificationCode)
-    if(token){
-      yield take('LOGOUT_REQUEST')
-      yield call(logoutRequest)
+    const task = yield fork(loginRequest, username, password, verificationCode)
+    const action = yield take(['LOGOUT_REQUEST','LOGIN_FAILED'])
+    if(action.type === 'LOGOUT_REQUEST'){
+      yield cancel(task)
     }
+    yield call(logoutRequest)
   }
 }
 
